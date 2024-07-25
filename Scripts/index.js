080ed68d6027cd08d54bc0c729a787a255964890
Nox <eduardo.s.souza@protonmail.com>
2024-07-25 14:33:30 -0300

    Implementation of equipment section initialized.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,5 @@
 import { rollDice } from "./dice.js"
-import { pcClass } from "./class.js";
+import { pcClass } from "./class.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     const rollButtons = document.querySelectorAll('.Attributes .rollButton');
@@ -9,33 +9,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const pcClassSelect = document.getElementById('pcClassSelect')
     const pcClassOptions = document.querySelectorAll('#pcClassSelect option')
     const hpRollButton = document.getElementById('hpRollButton')
+    const totalHpText = document.getElementById('totalHpText')
+    const goldRollButton = document.getElementById('goldRollButton')
 
+    let mod = 0
     let restrictedClasses = []
+    let initialGold = 0
 
     // Event responsible for handling attribute rolls
     rollButtons.forEach(rollButton => {
         // Initializes the roll count as zero
-        rollButton.rollCount = 0;
-        let mod = '';
+        rollButton.rollCount = 0
 
         rollButton.addEventListener('click', (ev) => {
-            ev.preventDefault();
-            const parentDiv = rollButton.closest('div.mb-4');
-            const inputField = parentDiv.querySelector('.attributeText');
+            ev.preventDefault()
+            const parentDiv = rollButton.closest('div.mb-3')
+            const inputField = parentDiv.querySelector('.attributeText')
             const modifier = parentDiv.querySelector('.Modifier')
 
             const currentValue = parseInt(inputField.value) || 0;
-            const rolledValue = rollDice(1, 6, 3);
+            const rolledValue = rollDice(1, 6, 3)
 
             if ((currentValue <= 8 || currentValue === 0) && rollButton.rollCount < 2) {
-                inputField.value = rolledValue;
-                rollButton.rollCount++;
+                inputField.value = rolledValue
+                rollButton.rollCount++
             } else if (currentValue > 8) {
-                window.alert('You cannot reroll a value larger than 8!');
-                currentValue = currentValue;
+                window.alert('You cannot reroll a value larger than 8!')
+                currentValue = currentValue
             } else if (rollButton.rollCount === 2) {
-                window.alert('You have already rerolled once!');
-                currentValue = currentValue;
+                window.alert('You have already rerolled once!')
+                currentValue = currentValue
             }
 
             // Values for Modifiers
@@ -71,9 +74,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     mod = 3
                     break
                 default:
-                    mod = ''
+                    mod = null
             }
-            modifier.innerText = 'Modifier: ' + (mod >= 0 ? '+' : '') + mod;
+            modifier.innerText = 'Modifier: ' + (mod >= 0 ? '+' : '') + mod
 
             // Resets all of the attributes values
             resetAttButton.addEventListener('click', () => {
@@ -81,10 +84,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 attributes.forEach(attribute => {
                     attribute.value = ''
                     modifier.innerText = 'Modifier: '
-                    rollButton.rollCount = 0;
+                    rollButton.rollCount = 0
                     restrictedClasses.length = 0
                 })
             })
+
+            console.log(inputField.value);
     })})
 
 resetAttButton.addEventListener('click', () => {
@@ -92,24 +97,22 @@ resetAttButton.addEventListener('click', () => {
         restrictedClassesText.hidden = true
         restrictedClassesText.innerText = 'Currently Restricted Classes: '
         pcClassOptions[i].disabled = false
+        hpInput.value = null
+        totalHpText.innerText ='Total Hit Points: '
     }
 })    
 
 // Helper function to disable a class and log it
 function restrictClass(className, optionIndex) {
     if (!restrictedClasses.includes(className)) {
-        restrictedClasses.push(className);
-        console.log(restrictedClasses);
-        pcClassSelect.options[optionIndex].disabled = true;
+        restrictedClasses.push(className)
+        console.log(restrictedClasses)
+        pcClassSelect.options[optionIndex].disabled = true
     }
 }
 
 // Event responsible for handling class restrictions
 pcClassSelect.addEventListener('click', () => {
-    const intValue = parseInt(document.getElementById('int').value, 10);
-    const conValue = parseInt(document.getElementById('con').value, 10);
-    const dexValue = parseInt(document.getElementById('dex').value, 10);
-    const wisValue = parseInt(document.getElementById('wis').value, 10);
 
     attributeInput.forEach(attribute => {
         const attributeId = attribute.getAttribute('id');
@@ -118,39 +121,35 @@ pcClassSelect.addEventListener('click', () => {
         if (attributeValue < 9 && attributeValue >= 3) {
             switch (attributeId) {
                 case 'dex':
-                    if (intValue < 9 && intValue >= 3) {
-                        restrictClass('Bard', 3);
-                    } else {
-                        restrictClass('Barbarian', 2);
-                    }
+                    restrictClass('Barbarian', 2)
+                    restrictClass('Bard', 3)
+                    restrictClass('Halfling', 13)
                     restrictClass('Illusionist', 15)
+                    restrictClass('Knight', 16)
                     break;
                 case 'int':
-                    restrictClass('Drow', 5);
-                    restrictClass('Elf', 9);
+                    restrictClass('Bard', 3)
+                    restrictClass('Drow', 5)
+                    restrictClass('Duergar', 7)
+                    restrictClass('Elf', 9)
                     break;
                 case 'con':
-                    if (intValue < 9 && intValue >= 3) {
-                        restrictClass('Duergar', 7);
-                    }
-                    if (dexValue < 9 && dexValue >= 3) {
-                        restrictClass('Halfling', 13)
-                        restrictClass('Knight', 16)
-                    }
-                    if (wisValue < 9 && wisValue >= 3) {
-                        restrictClass('Ranger', 19)
-                    }
-                    restrictClass('Dwarf', 8);
-                    restrictClass('Gnome', 11);
+                    restrictClass('Duergar', 7)
+                    restrictClass('Dwarf', 8)
+                    restrictClass('Gnome', 11)
+                    restrictClass('Half-Elf', 12)
+                    restrictClass('Halfling', 13)
+                    restrictClass('Knight', 16)
+                    restrictClass('Ranger', 19)
                     restrictClass('Svirfneblin', 20)
                     break;
                 case 'cha':
-                    if (conValue < 9 && conValue >= 3) {
-                        restrictClass('Half-Elf', 12);
-                    }
+                    restrictClass('Half-Elf', 12)
                     restrictClass('Paladin', 18)
                     break;
-                
+                case 'wis':
+                    restrictClass('Ranger', 19)
+                    break
             }
 
             if (restrictedClasses.length > 0) {
@@ -172,8 +171,10 @@ pcClassSelect.addEventListener('click', () => {
     // Event responsible for handling character HP roll
     hpRollButton.addEventListener('click', () => {
         hpRollButton.rollCount = 0;
+        const conModifier = document.getElementById('conModifier')
         let hpInput = document.getElementById('hpInput')
         let chosenClass = pcClassSelect.options[pcClassSelect.selectedIndex].value
+        
 
         // Object to map class names to their parameters
         const classParameters = {
@@ -202,16 +203,48 @@ pcClassSelect.addEventListener('click', () => {
         };
 
         function setHPForClass(chosenClass) {
-            const params = classParameters[chosenClass];
+            const params = classParameters[chosenClass]
             if (params) {
-                let characterClass = new pcClass(...params);
-                characterClass.calculateHP();
-                hpInput.value = characterClass.totalHP;
+                let characterClass = new pcClass(...params)
+                characterClass.calculateHP()
+                hpInput.value = characterClass.totalHP
             } else {
-                hpInput.value = '';
+                hpInput.value = ''
             }
         }
 
-        setHPForClass(chosenClass);
+        setHPForClass(chosenClass)
+
+        // Extracting the numerical value using a regex
+        let conModValueMatch = conModifier.innerText.match(/-?\d+/);
+
+        if (conModValueMatch) {
+            let conModValue = parseInt(conModValueMatch[0], 10); // Convert to integer
+
+            console.log(conModValue);
+
+            // Calculate total hit points
+            let hpTotal = parseInt(hpInput.value, 10) + conModValue;
+
+            if (hpTotal > 0) {
+                totalHpText.innerText = `Total Hit Points: ${hpTotal} (${conModValue})`;
+            } else if (hpTotal <= 0) {
+                hpTotal = 1;
+                totalHpText.innerText = `Total Hit Points: ${hpTotal} (${conModValue})`;
+            } else {
+                console.log("No numerical value found in conModifier text.");
+            }
+
+        }
     })
+
+    // Event responsible for handling initial gold
+    goldRollButton.addEventListener('click', () => {
+        const goldInput = document.getElementById('goldInput')
+        initialGold = (rollDice(1, 6, 3)) * 10
+        
+        goldInput.value = initialGold
+    })
+
+
 })
